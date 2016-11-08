f099380d2be0e2ffd2da9b8b683bfd5565af1ea2
Dana Woodman <dana@danawoodman.com>
2016-11-08 13:35:17 -0800

    Remove unnecessary bits.

--- karma.conf.js
+++ karma.conf.js
@@ -3,18 +3,6 @@
 const path = require('path')
 
 let webpackConfig = require('./webpack.config')
-//webpackConfig = Object.assign({}, webpackConfig, {
-  //// See: http://airbnb.io/enzyme/docs/guides/karma.html
-  //externals: {
-    //'cheerio': 'window',
-    //'react/addons': true,
-    //'react/lib/ExecutionEnvironment': true,
-    //'react/lib/ReactContext': true,
-  //},
-  //node: {
-    //fs: 'empty',
-  //},
-//})
 webpackConfig.entry = {}
 
 module.exports = (config) => {
@@ -32,7 +20,6 @@ module.exports = (config) => {
     files: [
       'node_modules/babel-polyfill/dist/polyfill.min.js',
       'node_modules/core-js/client/shim.min.js',
-      //'node_modules/whatwg-fetch/fetch.js',
       'src/**/*.test.js',
     ],
     preprocessors: {
